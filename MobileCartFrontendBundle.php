4b9fb49e1040f6d8cb519074c08e4ebd9b32ef5b
Jesse Hanson <jesse@mobilecart.io>
2017-02-08 22:24:35 -0500

    theme enhancements

--- MobileCartFrontendBundle.php
+++ MobileCartFrontendBundle.php
@@ -6,4 +6,21 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MobileCartFrontendBundle extends Bundle
 {
+
+    public function boot()
+    {
+        $this->container->get('cart.theme.config')
+            ->setTheme(
+                'frontend',
+                'MobileCartFrontendBundle::frontend-layout.html.twig',
+                'MobileCartFrontendBundle:',
+                'bundles/mobilecartfrontend'
+            )->setTheme(
+                'email',
+                'MobileCartFrontendBundle::email-layout.html.twig',
+                'MobileCartFrontendBundle:',
+                'bundles/mobilecartfrontend'
+            );
+    }
+
 }
